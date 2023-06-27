cb1be292df3ea10d6dcd85d757bfc7f9c9ae2a6e
Vladimir Belich <belichvladimir@mail.ru>
2023-06-27 15:28:51 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,166 @@
-import Card from "./card";
+class Card {
+	#el;
+	#styles;
+
+	constructor(element) {
+		this.#el = element;
+		this.#styles = window.getComputedStyle(element);
+	}
+
+	clear() {
+		this.#el.remove()
+	}
+
+	set styles(text) {
+		this.#el.style.cssText = text;
+	}
+
+	get styles() {
+		return this.#styles;
+	}
+
+	get proection() {
+		return (() => {
+			const d = document.createElement('div');
+			d.classList.add('proection');
+			const { width, height } = this.styles;
+			d.style.cssText = `
+	 			width: ${width};
+		 		height: ${height};
+		 		margin: 10px 0;
+			`
+			return d;
+		})();
+	}
+
+	static create(content) {
+		const node = document.createElement('div');
+		node.classList.add('draggable');
+		node.textContent = content;
+		const close = document.createElement('div');
+		close.classList.add('close');
+		close.innerHTML = '&#10060';
+		close.addEventListener("click", (e) => {
+			e.stopPropagation;
+			e.target.parentElement.remove(e.target);
+		  });
+		node.appendChild(close);
+		return new Card(node);
+	}
+
+	get element() {
+		return this.#el;
+	}
+}
+
+class Controller {
+	constructor(container) {
+		this.container = container;
+		// Card
+		this.draggingElement = null;
+		// Card.proection
+		this.draggingProection = null;
+	}
+
+	init() {
+		// this.container.append(Card.create(15).element)
+	}
+
+	setDraggingElement(node) {
+		this.draggingElement = new Card(node);
+	}
+
+	replaceDragging() {
+    // console.log('this.draggingElement.element',this.draggingElement.element)
+		this.draggingProection.replaceWith(this.draggingElement.element);
+		this.draggingElement.element.style = this.draggingElement.styles;
+	}
+
+	clear() {
+		this.draggingElement = null;
+		this.draggingProection = null;
+	}
+
+	onMouseDown = (evt) => {
+		const target = evt.target;
+		if (target.classList.contains('draggable')) {
+			this.shiftX = evt.offsetX;
+			this.shiftY = evt.offsetY;
+			this.setDraggingElement(target);
+			this.draggingElement.style = `
+		 		left: ${evt.pageX - this.shiftX}px;
+		 		top: ${evt.pageY - this.shiftY}px;
+			`
+			this.proectionAct(evt)
+		}
+	}
+
+	onMouseUp = () => {
+
+		if (this.draggingElement) {
+			this.replaceDragging();
+			this.clear()
+		}
+	}
+
+	// Рассчёт позиции вставки проекции и вставка или удаление
+	proectionAct(evt) {
+		const target = evt.target;
+		const element = this.draggingElement;
+		const proection = this.draggingProection;
+		if (
+			(target.classList.contains("draggable")) &&
+			!target.classList.contains("proection")
+		) {
+			const { y, height } = target.getBoundingClientRect();
+			const appendPosition = y + height / 2 > evt.clientY
+				? "beforebegin"
+				: "afterend";
+
+			if (!proection) {
+				this.draggingProection = element.proection;
+			} else {
+				proection.remove();
+				target.insertAdjacentElement(appendPosition, proection);
+			}
+		}
+		else if ((target.classList.contains("col")) &&
+		!target.classList.contains("proection")) {
+			const { y, height } = target.getBoundingClientRect();
+			const appendPosition = "beforeEnd";
+			if (!proection) {
+				this.draggingProection = element.proection;
+			} else {
+				proection.remove();
+				target.insertAdjacentElement(appendPosition, proection);
+			}
+		}
+	}
+	onMouseMove = (evt) => {
+		if (this.draggingElement) {
+		  const { pageX, pageY } = evt;
+		  const element = this.draggingElement;
+		  const { width, height } = this.draggingElement.styles;
+		  element.styles = `
+			position: absolute;
+			 left: ${pageX - this.shiftX}px;
+			 top: ${pageY - this.shiftY}px;
+			 pointer-events: none;
+			width: ${width};
+			height: ${height};
+		  `
+		  this.proectionAct(evt)
+		}
+	  }
+	  }
 
 const close = document.querySelectorAll(".close");
 document.querySelectorAll(".add-card").forEach((e) => {
   e.addEventListener("click", (event) => {
-    if (!event.target.nextElementSibling || event.target.nextElementSibling.className !== 'add-text') {
+    if (
+      !event.target.nextElementSibling ||
+      event.target.nextElementSibling.className !== "add-text"
+    ) {
       const input = document.createElement("input");
       input.className = "add-text";
       const btn = document.createElement("button");
@@ -28,76 +185,16 @@ document.querySelectorAll(".add-card").forEach((e) => {
   });
 });
 
-
-
 close.forEach((elem) => {
   elem.addEventListener("click", (e) => {
     e.stopPropagation;
     e.target.parentElement.remove(e.target);
   });
 });
-const items = document.querySelector('.todo')
+const controller = new Controller(document.querySelector(".col"));
 
-// const itemsElement = items.querySelectorAll('.draggable');
+controller.init();
 
-const itemsProgress = document.querySelector('.progress')
-
-// const itemsElementProgress = items.querySelectorAll('.draggable');
-
-const itemsDone = document.querySelector('.done')
-
-// const itemsElementDone = items.querySelectorAll('.draggable');
-
-let actualelement;
-let oldElem;
-
-const onMouseOver = (event) => {
-  // if (oldElem!==event.target && event.target.className==='draggable') {
-  //   console.log(event.target);
-  //   if (oldElem) oldElem.classList.remove('mouseover');
-  //   event.target.classList.add('mouseover');
-  //   oldElem =event.target;
-  // }
-  actualelement.style.top = event.clientY  + 'px';
-  actualelement.style.left = event.clientX + 'px';
-
-}
-const onMouseUp = (e) => {
-  const mouseUpItem = e.target;
-  mouseUpItem.classList.remove('mouseover');
-if (mouseUpItem.className==='col todo') items.appendChild(actualelement);
-if (mouseUpItem.className==='col progress') itemsProgress.appendChild(actualelement);
-if (mouseUpItem.className==='col done') itemsDone.appendChild(actualelement);
-if (mouseUpItem.className==='draggable' && mouseUpItem.parentElement.className==='col todo')  items.insertBefore(actualelement,mouseUpItem);
-if (mouseUpItem.className==='draggable' && mouseUpItem.parentElement.className==='col done')  itemsDone.insertBefore(actualelement,mouseUpItem);
-if (mouseUpItem.className==='draggable' && mouseUpItem.parentElement.className==='col progress')  itemsProgress.insertBefore(actualelement,mouseUpItem);
-  actualelement.classList.remove('dragged');
-  actualelement = undefined;
-  document.documentElement.removeEventListener('mouseup',onMouseUp)
-  document.documentElement.removeEventListener('mousemove',onMouseOver)  
-}
-
-items.addEventListener('mousedown',(e)=> {
-  e.preventDefault;
-  actualelement = e.target;
-  actualelement.classList.add('dragged');
-
-  document.documentElement.addEventListener('mouseup',onMouseUp)
-  document.documentElement.addEventListener('mousemove',onMouseOver)  
-})
-itemsProgress.addEventListener('mousedown',(e)=> {
-  e.preventDefault;
-  actualelement = e.target;
-  actualelement.classList.add('dragged');
-
-  document.documentElement.addEventListener('mouseup',onMouseUp)
-  document.documentElement.addEventListener('mousemove',onMouseOver)  
-})
-itemsDone.addEventListener('mousedown',(e)=> {
-  e.preventDefault;
-  actualelement = e.target;
-  actualelement.classList.add('dragged');
-
-  document.documentElement.addEventListener('mouseup',onMouseUp)
-  document.documentElement.addEventListener('mousemove',onMouseOver)  
-})
+document.body.addEventListener("mousedown", controller.onMouseDown);
+document.body.addEventListener("mouseup", controller.onMouseUp);
+document.body.addEventListener("mousemove", controller.onMouseMove);
